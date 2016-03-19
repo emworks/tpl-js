ef3b878ac509423af5bf1f939f0045243ccdeced
Yevgeny Yakushev <e.v.yakushev@gmail.com>
2016-03-20 02:57:10 +0300

    load data from json

--- tpl.js
+++ tpl.js
@@ -23,6 +23,10 @@
     tplValue: {
       name: 'value',
       binder: options.binder + 'value'
+    },
+    tplChecked: {
+      name: 'checked',
+      binder: options.binder + 'checked'
     }
   };
 
@@ -30,7 +34,7 @@
     ? module.exports = factory(w, options)
     : factory(w, options);
 
-}(typeof window !== "undefined" ? window : this, function(window, opt) {
+}(typeof window !== 'undefined' ? window : this, function(window, opt) {
 
   var map = [];
 
@@ -43,7 +47,7 @@
           : data[key] = value;
       },
       get: function(key) {
-        !!~key.indexOf(opt.notation)
+        return !!~key.indexOf(opt.notation)
           ? tpl.fn.parse(key.split(opt.notation), data)
           : data[key];
       },
@@ -64,13 +68,18 @@
         if (match = opt.regex.exec(where)) {
           var item = match[1].trim(),
               tplItem = tpl.get(item);
-          if (!tplItem) tpl.fn.parse(item.split(opt.notation), tpl.print());
+          if (!tplItem) {
+            tpl.fn.parse(item.split(opt.notation), tpl.print());
+            nodes[i].removeAttribute(opt.allowed[attr].name);
+          }
+          else {
+            (typeof nodes[i][opt.allowed[attr].name] !== 'undefined')
+              ? nodes[i][opt.allowed[attr].name] = tplItem
+              : nodes[i].setAttribute(opt.allowed[attr].name, tplItem);
+          }
           if (nodes[i].getAttribute(opt.binder + 'id') === null)
             nodes[i].setAttribute(opt.binder + 'id', map.length);
           nodes[i].setAttribute(opt.allowed[attr].binder, item);
-          (nodes[i][opt.allowed[attr].name])
-            ? nodes[i][opt.allowed[attr].name] = tplItem || " "
-            : nodes[i].setAttribute(opt.allowed[attr].name, tplItem || " ");
           items.push(item);
         }
       });
@@ -107,7 +116,7 @@
     request.onreadystatechange = function() {
       if (this.readyState !== 4) return;
       (this.status >= 200 && this.status < 400)
-        ? (json) ? responseText : fn(this.responseXML)
+        ? fn( (json) ? this.responseText : this.responseXML )
         : console.log(this);
     };
     request.send();
@@ -129,8 +138,24 @@
       if (!ref[items[i]]) ref[items[i]] = {};
       ref = ref[items[i]];
     }
-    ref[items[last]] = (value) ? value : ref[items[last]] || null;
+    ref[items[last]] = (typeof value !== 'undefined') ? value : ref[items[last]];
     return ref[items[last]];
+  };
+
+  tpl.fn.merge = function(target, data) {
+    var fn = function(target, data) {
+      var ref = target;
+      tpl.fn.walk(data, function(key) {
+        try {
+          (data[key].constructor === Object)
+            ? ref[key] = fn(ref[key], data[key])
+            : ref[key] = data[key];
+        } catch(e) {
+          ref[key] = data[key];
+        }
+      });
+    };
+    fn(target, data);
   };
 
   tpl.set = function(key, value) {
@@ -149,8 +174,8 @@
   tpl.render = function(item) {
     if (item.nodeName.toLowerCase() !== opt.el) return false;
     tpl.fn.request(item.id + opt.ext.data, function(response) {
-
-    });
+      tpl.fn.merge(tpl.print(), JSON.parse(response)[0]);
+    }, true);
     tpl.fn.request(item.id + opt.ext.view, function(response) {
       item.outerHTML = getTemplate(response);
     });
@@ -163,7 +188,9 @@
       if (!~item.indexOf(key)) return;
       var el = document.querySelector('[' + opt.binder + 'id="' + index + '"]');
       tpl.fn.walk(el.dataset, function(attr) {
-        if (el.dataset[attr] === key) el[opt.allowed[attr].name] = value;
+        if (el.dataset[attr] === key) (value)
+          ? el[opt.allowed[attr].name] = value
+          : el.removeAttribute(opt.allowed[attr].name);
       });
     });
   });
@@ -173,8 +200,24 @@
   });
 
   window.document.addEventListener('change', function(event) {
+    if (!event.target.dataset) return;
+    var data = {};
+    switch (event.target.type) {
+      case 'checkbox':
+        data.key = event.target.dataset.tplChecked;
+        data.value = event.target.checked;
+        break;
+      case 'text':
+        data.key = event.target.dataset.tplValue;
+        data.value = event.target.value;
+        break;
+      default:
+        data.key = event.target.dataset.tplValue;
+        data.value = event.target.value;
+        break;
+    }
     tpl.fn.pubsub.publish(
-      'view:changed', event.target.dataset.tplValue, event.target.value
+      'view:changed', data.key, data.value
     );
   });
 
