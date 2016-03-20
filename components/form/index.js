541a109f12d993361d2a923f35969b85ccd1e90e
Yevgeny Yakushev <e.v.yakushev@gmail.com>
2016-03-21 01:33:10 +0300

    sample counter btn

--- components/form/index.js
+++ components/form/index.js
@@ -2,4 +2,11 @@
 
   tpl.set('counter.checkbox.checked', true);
 
+  var counterInc = document.getElementById(tpl.get('counter.btn.id'));
+
+  counterInc.addEventListener('click', function() {
+    var value = tpl.get('counter.input.value');
+    tpl.set('counter.input.value', ++value);
+  });
+
 }());
